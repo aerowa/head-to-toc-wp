b28c096027b0cf67a3e1fd973601d742b126a377
aerowa <96546985+aerowa@users.noreply.github.com>
2023-07-23 22:07:57 +0100

    Update all-headers.php

--- all-headers.php
+++ all-headers.php
@@ -2,7 +2,7 @@
 /**
 * Plugin Name: ToC Header Box
 * Plugin URI: https://github.com/aerowa/head-to-toc-wp
-* Description: Assigns Head tags to links on selected pages as 'Table of Contents'. Please see readme on plugin URL. !Important! Customized for DailyAI: ALL headers are linked, and been stylized. You only need to change pageID under "array" and Reupload.
+* Description: Assigns Head tags to links on selected pages as 'Table of Contents'. Please see readme on plugin URL.
 * Version: 0.3.9
 * Author: Mimmikk.
 * Author URI: https://github.com/aerowa
